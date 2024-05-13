79ecfbc5b306cefb55ba78bbe771cc929926a462
Pradigta Hisyam Ramadhan <pradigta.phr@gmail.com>
2024-05-13 21:27:00 +0700

    update: header fungsi haversine

--- haversine.h
+++ haversine.h
@@ -2,6 +2,6 @@
 #ifndef HAVERSINE_H
 #define HAVERSINE_H
 
-double haversine(double lat1, double lon1, double lat2, double lon2);
+extern double haversine(double lat1, double lon1, double lat2, double lon2);
 
 #endif
